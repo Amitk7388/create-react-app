c0312fc18f972ed33aa5b0bc91ca51acceec5e4b
amitk7388 <amitk7388@gmail.com>
2020-01-02 00:05:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import Person from "./Person/Person"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component { 
+  state = {
+    persons : [
+      {name : "Max", age : 37},
+      {name : "Manu", age : 29},
+      {name : "stephnie", age : 37},
+    ]
+  }
+
+  swithcNameHandler = (newName) => {
+    this.setState({
+      persons : [
+        {name : newName, age : 25},
+        {name : "Manu", age : 29},
+        {name : "stephnie", age : 37},
+      ] 
+    })
+  }
+
+  nameChangeHandler = (event) => {
+    this.setState({
+      persons : [
+        {name : "max", age : 25},
+        {name : event.target.value, age : 29},
+        {name : "stephnie", age : 37},
+      ] 
+    })
+  }
+
+  render() {
+    const style = {
+      backgroundColor : "white",
+      font: "inherit",
+      border: "1px solid black",
+      padding: "8px",
+      corsor: "pointer"
+    }
+
+    return (
+      <div className="App">
+        <h1>Hi I am Amit kumar sahu</h1>
+        <p>My name is amit kumar sahu "clas no 07"</p>
+        <button style= {style} onClick ={ () => this.swithcNameHandler("Kushboo") }>Switch Name</button>
+        <Person
+         name = {this.state.persons[0].name} 
+         age = {this.state.persons[0].age} >
+         </Person>
+        <Person
+         name = {this.state.persons[1].name} 
+         age = {this.state.persons[1].age} 
+         onlcick = {this.swithcNameHandler.bind(this, "Max")}
+         onChanged = {this.nameChangeHandler} >My Hobbies: Racing
+         
+         </Person>
+        <Person 
+        name = {this.state.persons[2].name} 
+        age = {this.state.persons[2].age} >
+
+        </Person>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
