1ee17d66a38f7b74c64bd3a44fdbb463ea86a2fc
amitk7388 <amitk7388@gmail.com>
2020-01-02 01:35:07 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,70 +1,79 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Person from "./Person/Person"
 
-class App extends Component { 
+class App extends Component {
   state = {
-    persons : [
-      {name : "Max", age : 37},
-      {name : "Manu", age : 29},
-      {name : "stephnie", age : 37},
-    ]
+    persons: [
+      { name: "Max", age: 37 },
+      { name: "Manu", age: 29 },
+      { name: "stephnie", age: 37 },
+    ],
+    showPerson: false
   }
 
   swithcNameHandler = (newName) => {
     this.setState({
-      persons : [
-        {name : newName, age : 25},
-        {name : "Manu", age : 29},
-        {name : "stephnie", age : 37},
-      ] 
+      persons: [
+        { name: newName, age: 25 },
+        { name: "Manu", age: 29 },
+        { name: "stephnie", age: 37 },
+      ]
     })
   }
 
   nameChangeHandler = (event) => {
     this.setState({
-      persons : [
-        {name : "max", age : 25},
-        {name : event.target.value, age : 29},
-        {name : "stephnie", age : 37},
-      ] 
+      persons: [
+        { name: "max", age: 25 },
+        { name: event.target.value, age: 29 },
+        { name: "stephnie", age: 37 },
+      ]
     })
+  }
+
+  togglePersonHandler = () => {
+    const doesShow = this.state.showPerson
+    this.setState({ showPerson: !doesShow })
   }
 
   render() {
     const style = {
-      backgroundColor : "white",
+      backgroundColor: "white",
       font: "inherit",
       border: "1px solid black",
       padding: "8px",
       corsor: "pointer"
     }
 
+    let persons = null
+    if (this.state.showPerson) {
+      console.log("if condition")
+      persons = (
+        <div>
+          {
+            this.state.persons.map(person => {
+              console.log("here is ther persons", person)
+              return <Person 
+              name = {person.name} 
+              age = {person.age}
+              />
+            })
+          }
+        </div>
+      )
+    }
+
     return (
       <div className="App">
-        <h1>Hi I am Amit kumar sahu</h1>
+        <h1>Hi I am Amit kumar sahu Lecture 4.6</h1>
         <p>My name is amit kumar sahu "clas no 07"</p>
-        <button style= {style} onClick ={ () => this.swithcNameHandler("Kushboo") }>Switch Name</button>
-        <Person
-         name = {this.state.persons[0].name} 
-         age = {this.state.persons[0].age} >
-         </Person>
-        <Person
-         name = {this.state.persons[1].name} 
-         age = {this.state.persons[1].age} 
-         onlcick = {this.swithcNameHandler.bind(this, "Max")}
-         onChanged = {this.nameChangeHandler} >My Hobbies: Racing
-         
-         </Person>
-        <Person 
-        name = {this.state.persons[2].name} 
-        age = {this.state.persons[2].age} >
-
-        </Person>
+        <button style={style} onClick={this.togglePersonHandler}>Toggle Person</button>
+        {persons}
       </div>
     );
   }
-  
+
 }
 
 export default App;
